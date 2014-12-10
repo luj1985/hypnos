35f3359d9ef999e134ca6a9be2a434ce4f45a956
Lu, Jun <luj1985@gmail.com>
2014-12-10 23:25:05 +0800

    add semantic ui

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -2,6 +2,9 @@ Meteor.startup(function() {
   // TODO: add client side bootstrap code here
 });
 
-Template.layout.helpers({
-  
-});
+
+Template.layout.events({
+  'click .launch' : function(e) {
+    $('.left.sidebar').first().sidebar('toggle');
+  }
+})
