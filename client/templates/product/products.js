e1ff0e8571732dcc225a7e7c01dbe1caf012d719
Lu, Jun <luj1985@gmail.com>
2014-12-18 01:30:28 +0800

    add incremental search

--- client/templates/product/products.js
+++ client/templates/product/products.js
@@ -1,3 +1,17 @@
-Template.showproducts.events({
-  
+Template.searchProduct.events({
+  'keyup .search' : function(e) {
+    var search = $(e.target),
+        keyword = search.val();
+
+    if (keyword) {
+      PagedProducts.set('filters', {
+        $or : [
+          {sid: { $regex : keyword }},
+          {oid: { $regex : keyword }}
+        ]
+      });
+    } else {
+      PagedProducts.set('filters', {});
+    }
+  }
 });
