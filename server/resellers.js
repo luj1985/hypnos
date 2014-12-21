cfac0d4fc195006b81fd2b8b0bc6bcecbcccf0d3
Lu, Jun <luj1985@gmail.com>
2014-12-21 21:51:49 +0800

    use baidu place api to determine location

--- server/resellers.js
+++ server/resellers.js
@@ -1,15 +1,39 @@
+var BAIDU_PLACE_API_AK="EFjt6oKsiZ1lejOMPCQnE0DA";
+
 Meteor.startup(function() {
-  var CSV = Meteor.npmRequire('comma-separated-values');
   if (Resellers.find().count() === 0) {
+    var CSV = Meteor.npmRequire('comma-separated-values');
     var resellers = new CSV(Assets.getText("resellers.csv"), {
       header: true, cast: false, line: "\n"
     }).parse();
+
     _.each(resellers, function(reseller) {
-      var products = reseller.products.split(/[ \/，]/),
-          carlines = reseller.carlines.split(/[ \/，]/);
-      reseller.products = _.compact(products);
-      reseller.carlines = _.compact(carlines);
-      Resellers.insert(reseller);
+      HTTP.get('http://api.map.baidu.com/place/v2/search', {
+        params : {
+          q : reseller.address,
+          region : reseller.city,
+          output : "json",
+          ak : BAIDU_PLACE_API_AK
+        }
+      }, function(error, result) {
+        if (error) return console.log(error);
+
+        var suggestions = result.data.results || [];
+        var suggestion = suggestions[0] || {};
+        var location = suggestion.location;
+        
+        if (!location) {
+          console.warn('cannot find address: ' + reseller.city + ',' + reseller.address)
+        }
+
+        var products = reseller.products.split(/[ \/，]/),
+            carlines = reseller.carlines.split(/[ \/，]/);
+
+        reseller.products = _.compact(products);
+        reseller.carlines = _.compact(carlines);
+        reseller.location = location;
+        Resellers.insert(reseller);
+      });
     });
   }
 });
