08f0fa3ca0c890618a7d99cf7f297441b1ab489c
Lu, Jun <luj1985@gmail.com>
2014-12-23 20:53:17 +0800

    refactor to use meteor style

--- client/templates/reseller/resellers.js
+++ client/templates/reseller/resellers.js
@@ -1,26 +1,38 @@
+var LOCATION_KEY = 'location',
+    READY_FLAG = 'ready',
+    MESSAGE_KEY = 'locationError';
+
+Template.locationAccessor.helpers({
+  location : function() { return Session.get(LOCATION_KEY); }, 
+  message  : function() { return Session.get(MESSAGE_KEY); },
+  ready    : function() { return Session.get(READY_FLAG); }
+});
+
 Template.showResellers.events({
   'click #useLocation' : function() {
-    var message = $('#currentLocation')
-      .removeClass('error')
-      .html('<i class="ui inline mini active loader"></i>正在获取位置...')
-      .transition('fade in');
+    var message = $('#currentLocation').transition('fade in');
 
-    if (navigator.geolocation) {
+    if (navigator.geolocation) {   
+      Session.set(MESSAGE_KEY, null);
+      Session.set(READY_FLAG, false);
+
       navigator.geolocation.getCurrentPosition(function(p) {
-        var coords = [p.coords.latitude, p.coords.longitude];
-        message.text('当前位置：' + coords.join(','));
-        Session.set('current-coords', coords);
-      }, function(error) {
-        message.addClass('error').text(error.message);
+        Session.set(LOCATION_KEY, [p.coords.latitude, p.coords.longitude]);
+        Session.set(READY_FLAG, true);
+      }, function(e) {
+        Session.set(MESSAGE_KEY, e.message);
+      }, {
+        enableHighAcuracy: true,
+        timeout: 10000
       });
     } else {
-      message.addClass('error').text('不支持地理位置');
+      Session.set(MESSAGE_KEY, '不支持地理位置');
     }
   }
 });
 
 Deps.autorun(function() {
-  var coords = Session.get('current-coords');
+  var coords = Session.get(LOCATION_KEY);
   if (coords) {
     PagedResellers.set('filters',  { location : { $near : coords } });
   }
