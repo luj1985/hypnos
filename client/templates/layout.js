b2aa21601c5d8129bf3c6b4327d88525a33f8fc3
Lu, Jun <luj1985@gmail.com>
2014-12-27 19:36:33 +0800

    smooth animation

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,15 +1,16 @@
 Template.layout.rendered = function () {
-  var header = this.find('header.menu');
-  $(header).waypoint(function(direction) {
+  this.$('header.menu').waypoint(function(direction) {
   }, {
     offset : -20
   });
 
-  $('.sidebar').sidebar('attach events', '.item.launch');
+  this.$('.sidebar').sidebar('attach events', '.item.launch');
 };
 
 Template.layout.events({
   'click .sidebar > a.item' : function() {
-    $('.sidebar').sidebar('toggle');
+    Meteor.setTimeout(function() {
+      $('.sidebar:first').sidebar('toggle');
+    }, 100);
   }
 });
--- client/templates/reseller/resellers.js
+++ client/templates/reseller/resellers.js
@@ -8,29 +8,6 @@ Template.locationAccessor.helpers({
   ready    : function() { return Session.get(READY_FLAG); }
 });
 
-Template.showResellers.events({
-  'click #useLocation' : function() {
-    var message = $('#currentLocation').transition('fade in');
-
-    if (navigator.geolocation) {   
-      Session.set(MESSAGE_KEY, null);
-      Session.set(READY_FLAG, false);
-
-      navigator.geolocation.getCurrentPosition(function(p) {
-        Session.set(LOCATION_KEY, [p.coords.latitude, p.coords.longitude]);
-        Session.set(READY_FLAG, true);
-      }, function(e) {
-        Session.set(MESSAGE_KEY, e.message);
-      }, {
-        enableHighAcuracy: true,
-        timeout: 10000
-      });
-    } else {
-      Session.set(MESSAGE_KEY, '不支持地理位置');
-    }
-  }
-});
-
 Deps.autorun(function() {
   var coords = Session.get(LOCATION_KEY);
   if (coords) {
