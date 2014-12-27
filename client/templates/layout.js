8408aa2a312a9b5444cc1c17663d2c955e8bd1b6
Lu, Jun <luj1985@gmail.com>
2014-12-27 16:05:22 +0800

    add fixed header

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,21 +1,15 @@
 Template.layout.rendered = function () {
   var header = this.find('header.menu');
   $(header).waypoint(function(direction) {
-    // if (direction == 'down') {
-    //   $(this).addClass('fixed');
-    // } else {
-    //   $(this).removeClass('fixed');
-    // }
   }, {
     offset : -20
   });
+
+  $('.sidebar').sidebar('attach events', '.item.launch');
 };
 
 Template.layout.events({
-  'click .launch': function () {
-    $('.sidebar').sidebar('show');
-  },
   'click .sidebar > a.item' : function() {
-    $('.sidebar').sidebar('hide');
+    $('.sidebar').sidebar('toggle');
   }
 });
