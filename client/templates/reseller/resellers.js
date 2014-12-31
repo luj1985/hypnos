5a3e7acb863a061cb88429440ebfdf4c11f308c5
Lu, Jun <luj1985@gmail.com>
2014-12-31 16:09:15 +0800

    add cordova map uri

--- client/templates/reseller/resellers.js
+++ client/templates/reseller/resellers.js
@@ -2,6 +2,26 @@ var LOCATION_KEY = 'location',
     READY_FLAG = 'ready',
     MESSAGE_KEY = 'locationError';
 
+var mapAccessor = (function(){
+  if (Meteor.isCordova) {
+    var platform = (device.platform || "").toLowerCase();
+    switch(platform) {
+      case 'android' : 
+        return function(address, location) {
+          return "geo:" + location.lat + ',' + location.lng; 
+        }
+      case 'ios' :
+        return function(address, gps) { 
+          return "maps:q=" + address; 
+        }
+    }
+  } else {
+    return function(address, location) {
+      return "http://api.map.baidu.com/geocoder?address=" + encodeURIComponent(address) + "&output=html";
+    }
+  }
+})();
+
 Template.locationAccessor.helpers({
   location : function() { return Session.get(LOCATION_KEY); }, 
   message  : function() { return Session.get(MESSAGE_KEY); },
@@ -14,6 +34,9 @@ Template.reseller.helpers({
     return location ? _.map([location.lat, location.lng], function(f) {
       return parseFloat(f).toFixed(4);
     }).join(', ') : '';
+  },
+  href: function() {
+    return mapAccessor(this.address, this.location);
   }
 });
 Deps.autorun(function() {
