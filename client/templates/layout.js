c44f41af0f24234f70be56f816354ecae7b5f5a9
Lu, Jun <luj1985@gmail.com>
2015-01-01 23:44:29 +0800

    add sidebar swipe support

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -2,6 +2,19 @@ var SIDEBAR_STATUS_KEY = 'show-sidebar';
 Template.layout.rendered = function () {
   // HACK: to make sure that html body can fullfil the screen
   this.$('main').css('min-height', window.innerHeight);
+
+  var fromEdge = false;
+  $(document).on('touchstart', function(e) {
+     var xPos = e.originalEvent.touches[0].pageX;
+     fromEdge = xPos < 3;
+  });
+
+  $(document).hammer().on('drag', function(e){
+    // check that the drag event started at the edge and that the direction is to the right
+    if(fromEdge && e.gesture.direction === 'right'){
+      Session.set(SIDEBAR_STATUS_KEY, true);
+    }
+  });
 };
 
 Template.layout.helpers({
