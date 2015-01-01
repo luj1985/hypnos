9301ae350b3b5097b80f8341f5abb3eee840ab96
Lu, Jun <luj1985@gmail.com>
2015-01-01 22:20:01 +0800

    reset scroll position

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,2 +1,14 @@
 Meteor.startup(function() {
+  Meteor.Router.filters({
+    resetScroll: function(page) {
+      if (!(page === Session.get('Router.lastPage'))) {
+        Session.set('Router.lastPage', page);
+        Meteor.startup(function() {
+          window.scrollTo(0,0)
+        });
+      }
+      return page;
+    }
+  });
+  Meteor.Router.filter('resetScroll'); 
 });
