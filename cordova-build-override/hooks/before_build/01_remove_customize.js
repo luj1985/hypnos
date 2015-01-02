66a06aa58e57e16d4020255540d72583f7ce8f48
Lu, Jun <luj1985@gmail.com>
2015-01-02 14:07:26 +0800

    fix building hook scripts

--- cordova-build-override/hooks/before_build/01_remove_customize.js
+++ cordova-build-override/hooks/before_build/01_remove_customize.js
@@ -4,11 +4,10 @@ var fs = require('fs');
 var path = require('path');
 var rootdir = process.argv[2];
 
-// the build folder name will change when disabled 'custom-build'
-var target = path.join(rootdir, 'platforms', 'android', 'ant-build');
-var source = path.join(rootdir, 'platforms', 'android', 'bin');
+// crosswalk doesn't work after apply 'custom_rules'
+var target = path.join(rootdir, 'platforms', 'android', 'custom_rules.xml');
 
-if (fs.existsSync(source) && !fs.existsSync(target)) {
-  fs.symlinkSync(source, target);
-  console.log(target + ' has been created');
+if (fs.existsSync(target)) {
+  fs.unlinkSync(target);
+  console.log(target + ' has been removed');
 }
