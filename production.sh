55c70ee3c317f5d869b0db1fd811079da3061e19
Lu, Jun <luj1985@gmail.com>
2015-01-24 17:46:09 +0800

    resize header to suite android 4.3

--- production.sh
+++ production.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-export MONGO_URL="mongodb://127.0.0.1:27017/schaeffler"
-export WEIBO_KEY="1557510073"
-export ROOT_URL="http://121.40.34.211:3000"
-export METEOR_SETTINGS="$(cat settings.json)"
-
+source env.sh
 meteor --production $@
