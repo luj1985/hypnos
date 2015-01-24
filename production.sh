ac70bc1a5ff280dc169ad65dc9fea29cd9483747
Lu, Jun <luj1985@gmail.com>
2015-01-24 17:31:06 +0800

    add production start shell

--- production.sh
+++ production.sh
@@ -4,3 +4,5 @@ export MONGO_URL="mongodb://127.0.0.1:27017/schaeffler"
 export WEIBO_KEY="1557510073"
 export ROOT_URL="http://121.40.34.211:3000"
 export METEOR_SETTINGS="$(cat settings.json)"
+
+meteor --production $@ &
