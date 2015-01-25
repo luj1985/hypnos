098db129d2cbd11d427b95baebedaec6836201f7
Lu, Jun <luj1985@gmail.com>
2015-01-25 17:56:58 +0800

    add city jquery cityselect support

--- client/templates/reseller/search.js
+++ client/templates/reseller/search.js
@@ -1,3 +1,10 @@
+Template.resellerSearch.rendered = function () {
+  this.$('.location').citySelect({
+    nodata:"none",
+    required:false
+  });
+};
+
 Template.resellerSearch.events({
   'click input[type="submit"]': function (e, template) {
     e.preventDefault();
