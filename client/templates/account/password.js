2cd6a47f19795445d2cfce7d0df336a1ce012883
Lu, Jun <luj1985@gmail.com>
2015-01-27 01:50:17 +0800

    add error check

--- client/templates/account/password.js
+++ client/templates/account/password.js
@@ -33,6 +33,16 @@ Template.register.events({
     user.email = email;
     user.password = password;
 
+    var errors = {};
+    if (!username) { errors.username = ['请输入用户名']; }
+    if (!email) { errors.email = ['请输入邮件地址']; }
+    if (!password) { errors.password = ['请输入密码']; }
+    Session.set('errors', errors);
+
+    if (_.keys(errors).length) {
+      return;
+    }
+
     Accounts.createUser(user, function (err) {
       if (err) {
         accountService.message(err);
@@ -43,14 +53,33 @@ Template.register.events({
   }
 });
 
+Template.login.helpers({
+  errors: function() {
+    return Session.get('errors');
+  },
+  fieldClass: function(name) {
+    var errors = Session.get('errors');
+    return errors[name] ? 'invalid' : '';
+  }
+});
+
 Template.login.events({
   'submit form.login': function (e, template) {
     e.preventDefault();
-    var name = template.$('input[name="username"]').val(),
+    var username = template.$('input[name="username"]').val(),
         password = template.$('input[name="password"]').val();
 
+    var errors = {};
+    if (!username) { errors.username = ['请输入用户名']; }
+    if (!password) { errors.password = ['请输入密码']; }
+    Session.set('errors', errors);
+
+    if (_.keys(errors).length) {
+      return;
+    }
+
     // name can be username or email
-    Meteor.loginWithPassword(name, password, function(err) {
+    Meteor.loginWithPassword(username, password, function(err) {
       if (err) {
         accountService.message(err);
       }
