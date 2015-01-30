1e1ccac8fc8dceecf172aa9495f79a0b137db03a
Lu, Jun <luj1985@gmail.com>
2015-01-30 23:51:26 +0800

    move weibo data access to client

--- client/templates/account/weibo.js
+++ client/templates/account/weibo.js
@@ -1,7 +1,20 @@
-function success(data) {
-  Meteor.call('createUserFromWeibo', data, function(err, result) {
-    console.log(arguments);
+function success(auth) {
+  var uid = auth.uid, token = auth.token;
+  HTTP.get('https://api.weibo.com/2/users/show.json', {
+    params: { uid: uid, access_token: token }
+  }, function(error, resp) {
+    if (error) {
+      throwError(error);
+    } else {
+      var data = resp.data;
+      console.log(data);
+    }
   });
+  HTTP.get('https://api.weibo.com/2/account/profile/email.json', {
+    params: { uid: uid, access_token: token }
+  }, function(error, resp) {
+    console.log(arguments);
+  })
 }
 
 function failure(message) {
--- server/accounts.js
+++ server/accounts.js
@@ -16,15 +16,5 @@ Meteor.methods({
     return Meteor.users.update(Meteor.userId, {
       $set : { profile : profile }
     });
-  },
-  createUserFromWeibo: function(data) {
-    console.log('received access token');
-    console.log(data);
-    var uid = data.uid, token = data.token;
-    HTTP.get('https://api.weibo.com/2/users/show.json', {
-      params: { uid: uid, access_token: token }
-    }, function() {
-      console.log(arguments);
-    });
   }
 });
