fd320dd3df545f24c48d731f1ea312a3c495c487
Lu, Jun <luj1985@gmail.com>
2015-01-31 22:01:07 +0800

    refactor to make inappbrowser only eval on cordova env

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -11,23 +11,20 @@ Meteor.startup(function() {
   T9n.setLanguage("zh-cn");
 
   document.addEventListener('deviceready', function() {
-    $(document).on('click', 'a,button,input[type="button"]', function() {
+    $.expr[':'].external = function(obj){
+      var href = obj.href;
+      return href 
+        && !href.match(/^(mailto|tel|geo|maps)\:/)
+        && (obj.hostname != location.hostname);
+    };
+
+    $(document).on('click', 'a:external', function(e) {
+      e.preventDefault();
+      window.open($(this).attr('href'), '_system');
+    });
+
+    $(document).on('click', 'a,button,input[type="button"],input[type="submit"]', function() {
       plugins.deviceFeedback.acoustic();
     });
   }, false);
-
-  // Creating custom :external selector
-  $.expr[':'].external = function(obj){
-    var href = obj.href;
-    return href 
-      && !href.match(/^(mailto|tel|geo|maps)\:/)
-      && (obj.hostname != location.hostname);
-  };
-
-  $(document).on('click', 'a:external', function(e) {
-    if (Meteor.isCordova) {
-      e.preventDefault();
-      window.open($(this).attr('href'), '_system');
-    }
-  });
 });
