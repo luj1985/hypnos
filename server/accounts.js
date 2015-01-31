2246d827dafdb66fe7eea1385b3bd3f5430db0db
Lu, Jun <luj1985@gmail.com>
2015-01-31 15:00:46 +0800

    Can't call setUserId in a method after calling unblock

--- server/accounts.js
+++ server/accounts.js
@@ -20,7 +20,6 @@ Meteor.methods({
   },
   loginOrCreateAccountViaWeibo: function(data) {
     // TODO: add parameter check
-    this.unblock();
     var uid = data.uid, token = data.token;
     var result = HTTP.get('https://api.weibo.com/2/users/show.json', {
       params: { uid: uid, access_token: token } 
