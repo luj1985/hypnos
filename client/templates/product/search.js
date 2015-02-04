21028629edca4030dec180ed3504e34ca0a078d7
Lu, Jun <luj1985@gmail.com>
2015-02-04 23:08:00 +0800

    refactor product search page, make it be able to back

--- client/templates/product/search.js
+++ client/templates/product/search.js
@@ -14,55 +14,6 @@ Template.doProductSearch.events({
   }
 });
 
-Template.manufacturers.helpers({
-  options: function () {
-    return _.chain(Manufacturers.find().fetch())
-            .pluck('manufacturer')
-            .uniq()
-            .sort(function(s1, s2) { return s1.localeCompare(s2, 'zh'); })
-            .map(function(v) { return {name: v}; })
-            .value();
-  }
-});
-
-Template.manufacturer.events({
-  'click a': function (e) {
-    e.preventDefault();
-    Session.set('manufacturer', this.name);
-    Session.set('product-search-form', 'aliases');
-  }
-});
-
-Template.aliases.helpers({
-  options: function () {
-    var manufacturer = Session.get('manufacturer');
-    var ms = Manufacturers.find({manufacturer: manufacturer});
-    return _.chain(ms.fetch())
-            .pluck('alias')
-            .uniq()
-            .sort(function(s1, s2) { return s1.localeCompare(s2, 'zh'); })
-            .map(function(v) { return {name: v}; })
-            .value();
-  }
-});
-
-Template.alias.events({
-  'click a': function (e) {
-    e.preventDefault();
-    Session.set('alias', this.name);
-    Session.set('product-search-form', 'types');
-  }
-});
-
-Template.types.helpers({
-  options: function () {
-    var manufacturer = Session.get('manufacturer');
-    var alias = Session.get('alias');
-    var ms = Manufacturers.findOne({manufacturer: manufacturer, alias: alias});
-    return ms.types;
-  }
-});
-
 Template.types.events({
   'click a': function (e) {
     e.preventDefault();
